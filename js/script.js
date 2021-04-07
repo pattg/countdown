31e2ed1dc9192b21807e4d0100333364d362dc17
patmat <patrik.gaertner@gmail.com>
2021-04-07 22:33:24 +0200

    fix if statement to check if time is up

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ var hours = document.querySelector('.hours');
 var minutes = document.querySelector('.minutes');
 var seconds = document.querySelector('.seconds');
 var randomText = document.querySelector('.countdown__random-text');
+var countdown = document.querySelector('.countdown');
 var coundownWrapper = document.querySelector('.countdown__wrapper');
 var countdownModal = document.querySelector('.countdown__modal');
 console.log(countdownModal);
-var endDate = new Date('April 7, 2021 21:56:00').getTime();
+var endDate = new Date('April 14, 2021 20:30:00').getTime();
 var textArray = [
     'random stuff',
     'Wenn du wüsstest',
@@ -23,20 +24,36 @@ setInterval(function () {
 function setCountdown() {
     var now = new Date().getTime();
     var diff = endDate - now;
-    days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
-    hours.innerHTML = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
-    minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    if ((Math.floor(diff / (1000 * 60 * 60 * 24)) < 10)) {
+        days.innerHTML = "0" + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+    }
+    else {
+        days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+    }
+    if ((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) < 10) {
+        hours.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+    }
+    else {
+        hours.innerHTML = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+    }
+    if ((diff % (1000 * 60 * 60)) / (1000 * 60) < 10) {
+        minutes.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    }
+    else {
+        minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    }
     if (Math.floor((diff % (1000 * 60)) / 1000) < 10) {
         seconds.innerHTML = '0' + Math.floor((diff % (1000 * 60)) / 1000);
     }
     else {
         seconds.innerHTML = Math.floor((diff % (1000 * 60)) / 1000).toString();
     }
-    if ((parseInt(days.innerHTML)) && (parseInt(hours.innerHTML) && (parseInt(minutes.innerHTML) && (parseInt(seconds.innerHTML)) <= 0))) {
+    if (((parseInt(days.innerHTML) <= 0) && (parseInt(hours.innerHTML) <= 0) && ((parseInt(minutes.innerHTML) <= 0) && ((parseInt(seconds.innerHTML))) <= 0))) {
         coundownWrapper.classList.add('hidden');
         randomText.classList.add('hidden');
         countdownModal.classList.remove('hidden');
         countdownModal.classList.add('visible');
+        countdown.style.backgroundColor = "black";
     }
 }
 //random Text
--- script.ts
+++ script.ts
@@ -3,12 +3,13 @@ let hours = document.querySelector('.hours');
 let minutes = document.querySelector('.minutes');
 let seconds = document.querySelector('.seconds');
 let randomText = document.querySelector('.countdown__random-text');
+let countdown = document.querySelector<HTMLElement>('.countdown');
 let coundownWrapper = document.querySelector('.countdown__wrapper');
 let countdownModal= document.querySelector('.countdown__modal');
 
 console.log(countdownModal);
 
-let endDate = new Date('April 7, 2021 21:56:00').getTime();
+let endDate = new Date('April 14, 2021 20:30:00').getTime();
 const textArray = [
 	'random stuff',
 	'Wenn du wüsstest',
@@ -28,11 +29,33 @@ function setCountdown(): void {
 	let now = new Date().getTime();
 	let diff = endDate - now;
 
-	days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
-	hours.innerHTML = Math.floor(
-		(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-	).toString();
-	minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+	if((Math.floor(diff / (1000 * 60 * 60 * 24)) < 10 )) {
+		
+		days.innerHTML = "0" + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+	} else {
+		days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+	}
+
+	if((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) < 10) {
+
+		hours.innerHTML = "0" + Math.floor(
+			(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		).toString();
+	} else {
+
+		hours.innerHTML = Math.floor(
+			(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		).toString();
+	}
+
+	if((diff % (1000 * 60 * 60)) / (1000 * 60) < 10 ){
+
+		minutes.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+	} else {
+
+		minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+	}
+
 
 	if (Math.floor((diff % (1000 * 60)) / 1000) < 10) {
 		seconds.innerHTML = '0' + Math.floor((diff % (1000 * 60)) / 1000);
@@ -40,11 +63,12 @@ function setCountdown(): void {
 		seconds.innerHTML = Math.floor((diff % (1000 * 60)) / 1000).toString();
 	}
 
-	if((parseInt(days.innerHTML)) && (parseInt(hours.innerHTML) && (parseInt(minutes.innerHTML) && (parseInt(seconds.innerHTML)) <= 0 ))){
+	if(((parseInt(days.innerHTML) <= 0 ) && (parseInt(hours.innerHTML) <= 0) && ((parseInt(minutes.innerHTML) <= 0 ) && ((parseInt(seconds.innerHTML))) <= 0 ))){
 		coundownWrapper.classList.add('hidden');
 		randomText.classList.add('hidden');
 		countdownModal.classList.remove('hidden');
 		countdownModal.classList.add('visible');
+		countdown.style.backgroundColor = "black";
 	}
 }
 
