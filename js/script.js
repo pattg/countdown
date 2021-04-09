fc53e0197e2b911663493b2c840725a98355e8d6
patmat <patrik.gaertner@gmail.com>
2021-04-09 22:14:55 +0200

    add jQuery, add animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//TODO - randomize randomText function
+//TODO - add animations to modal (fade in/ fade out)
 var days = document.querySelector('.days');
 var hours = document.querySelector('.hours');
 var minutes = document.querySelector('.minutes');
@@ -6,38 +8,37 @@ var randomText = document.querySelector('.countdown__random-text');
 var countdown = document.querySelector('.countdown');
 var coundownWrapper = document.querySelector('.countdown__wrapper');
 var countdownModal = document.querySelector('.countdown__modal');
-console.log(countdownModal);
-var endDate = new Date('April 14, 2021 20:30:00').getTime();
+var endDate = new Date('April 14, 2021 21:00:00').getTime();
 var textArray = [
-    'random stuff',
-    'Wenn du wüsstest',
-    '172865',
-    'bli bla blub',
+    'Schnapp dir Stift und Zettel',
+    'Zieh dir etwas bequemes an',
+    'Es wird spannend',
 ];
 setInterval(function () {
     setCountdown();
 }, 1000);
-setInterval(function () {
-    setRandomText();
-}, 5000);
+setText();
 // Countdown function
 function setCountdown() {
     var now = new Date().getTime();
     var diff = endDate - now;
-    if ((Math.floor(diff / (1000 * 60 * 60 * 24)) < 10)) {
-        days.innerHTML = "0" + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+    if (Math.floor(diff / (1000 * 60 * 60 * 24)) < 10) {
+        days.innerHTML = '0' + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
     }
     else {
         days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
     }
     if ((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) < 10) {
-        hours.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+        hours.innerHTML =
+            '0' +
+                Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
     }
     else {
         hours.innerHTML = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
     }
     if ((diff % (1000 * 60 * 60)) / (1000 * 60) < 10) {
-        minutes.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+        minutes.innerHTML =
+            '0' + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
     }
     else {
         minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
@@ -48,16 +49,38 @@ function setCountdown() {
     else {
         seconds.innerHTML = Math.floor((diff % (1000 * 60)) / 1000).toString();
     }
-    if (((parseInt(days.innerHTML) <= 0) && (parseInt(hours.innerHTML) <= 0) && ((parseInt(minutes.innerHTML) <= 0) && ((parseInt(seconds.innerHTML))) <= 0))) {
+    if (parseInt(days.innerHTML) <= 0 &&
+        parseInt(hours.innerHTML) <= 0 &&
+        parseInt(minutes.innerHTML) <= 0 &&
+        parseInt(seconds.innerHTML) <= 0) {
         coundownWrapper.classList.add('hidden');
         randomText.classList.add('hidden');
         countdownModal.classList.remove('hidden');
         countdownModal.classList.add('visible');
-        countdown.style.backgroundColor = "black";
+        countdown.style.backgroundColor = 'black';
     }
 }
-//random Text
-function setRandomText() {
-    var randomNumber = Math.floor(Math.random() * textArray.length);
-    randomText.innerHTML = textArray[randomNumber];
+function setText() {
+    // let displayIndex: number = 0;
+    // let delay: number = 10000;
+    // setInterval(() => {
+    // 	if (randomText) {
+    // 		randomText.innerHTML = textArray[displayIndex];
+    // 	}
+    // 	// Move to the next item in dialog
+    // 	displayIndex++;
+    // 	// If display index goes out of index range, start again
+    // 	if (displayIndex >= textArray.length) {
+    // 		displayIndex = 0;
+    // 	}
+    // }, delay);
+    var count = 0;
+    setInterval(function () {
+        count++;
+        $('.countdown__random-text').fadeOut(2000, function () {
+            $(this)
+                .text(textArray[count % textArray.length])
+                .fadeIn(2000);
+        });
+    }, 10000);
 }
--- script.ts
+++ script.ts
@@ -5,57 +5,50 @@ let seconds = document.querySelector('.seconds');
 let randomText = document.querySelector('.countdown__random-text');
 let countdown = document.querySelector<HTMLElement>('.countdown');
 let coundownWrapper = document.querySelector('.countdown__wrapper');
-let countdownModal= document.querySelector('.countdown__modal');
+let countdownModal = document.querySelector('.countdown__modal');
 
-console.log(countdownModal);
-
-let endDate = new Date('April 14, 2021 20:30:00').getTime();
+let endDate = new Date('April 14, 2021 21:00:00').getTime();
 const textArray = [
-	'random stuff',
-	'Wenn du wüsstest',
-	'172865',
-	'bli bla blub',
+	'Schnapp dir Stift und Zettel',
+	'Zieh dir etwas bequemes an',
+	'Es wird spannend',
 ];
 
 setInterval(function () {
 	setCountdown();
 }, 1000);
-setInterval(function () {
-	setRandomText();
-}, 5000);
+
+setText();
 
 // Countdown function
 function setCountdown(): void {
 	let now = new Date().getTime();
 	let diff = endDate - now;
 
-	if((Math.floor(diff / (1000 * 60 * 60 * 24)) < 10 )) {
-		
-		days.innerHTML = "0" + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+	if (Math.floor(diff / (1000 * 60 * 60 * 24)) < 10) {
+		days.innerHTML = '0' + Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
 	} else {
 		days.innerHTML = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
 	}
 
-	if((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) < 10) {
-
-		hours.innerHTML = "0" + Math.floor(
-			(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-		).toString();
+	if ((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) < 10) {
+		hours.innerHTML =
+			'0' +
+			Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
 	} else {
-
 		hours.innerHTML = Math.floor(
 			(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
 		).toString();
 	}
 
-	if((diff % (1000 * 60 * 60)) / (1000 * 60) < 10 ){
-
-		minutes.innerHTML = "0" + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+	if ((diff % (1000 * 60 * 60)) / (1000 * 60) < 10) {
+		minutes.innerHTML =
+			'0' + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
 	} else {
-
-		minutes.innerHTML = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+		minutes.innerHTML = Math.floor(
+			(diff % (1000 * 60 * 60)) / (1000 * 60)
+		).toString();
 	}
-
 
 	if (Math.floor((diff % (1000 * 60)) / 1000) < 10) {
 		seconds.innerHTML = '0' + Math.floor((diff % (1000 * 60)) / 1000);
@@ -63,17 +56,45 @@ function setCountdown(): void {
 		seconds.innerHTML = Math.floor((diff % (1000 * 60)) / 1000).toString();
 	}
 
-	if(((parseInt(days.innerHTML) <= 0 ) && (parseInt(hours.innerHTML) <= 0) && ((parseInt(minutes.innerHTML) <= 0 ) && ((parseInt(seconds.innerHTML))) <= 0 ))){
+	if (
+		parseInt(days.innerHTML) <= 0 &&
+		parseInt(hours.innerHTML) <= 0 &&
+		parseInt(minutes.innerHTML) <= 0 &&
+		parseInt(seconds.innerHTML) <= 0
+	) {
 		coundownWrapper.classList.add('hidden');
 		randomText.classList.add('hidden');
 		countdownModal.classList.remove('hidden');
 		countdownModal.classList.add('visible');
-		countdown.style.backgroundColor = "black";
+		countdown.style.backgroundColor = 'black';
 	}
 }
 
-//random Text
-function setRandomText() {
-	let randomNumber = Math.floor(Math.random() * textArray.length);
-	randomText.innerHTML = textArray[randomNumber];
+function setText() {
+	// let displayIndex: number = 0;
+	// let delay: number = 10000;
+
+	// setInterval(() => {
+	// 	if (randomText) {
+	// 		randomText.innerHTML = textArray[displayIndex];
+	// 	}
+
+	// 	// Move to the next item in dialog
+	// 	displayIndex++;
+
+	// 	// If display index goes out of index range, start again
+	// 	if (displayIndex >= textArray.length) {
+	// 		displayIndex = 0;
+	// 	}
+	// }, delay);
+	let count = 0;
+
+	setInterval(function () {
+		count++;
+		$('.countdown__random-text').fadeOut(2000, function () {
+			$(this)
+				.text(textArray[count % textArray.length])
+				.fadeIn(2000);
+		});
+	}, 10000);
 }
